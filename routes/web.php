18dd6fce30b983c6b5673548b3ebcb2a2f5d67f6
narantyo maulana <olandeoxys@gmail.com>
2023-03-29 12:39:03 +0700

    Add DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,12 +22,17 @@ Route::get('/checkout/success',[FrontendController::class, 'success'])->name('ch
 
 
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+// Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+Route::middleware(['auth:sanctum','verified'])->name('dashboard.')->prefix('dashboard')->group(function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('index'); //dashboard.index cara memanggilnya
+    Route::middleware(['admin',])->group(function(){
+        
+    });
 });
+
+
