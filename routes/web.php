cf198efbf5a0bcdcf2ea67a5a448d48b0b0b2ca8
narantyo maulana <olandeoxys@gmail.com>
2023-03-29 11:43:44 +0700

    Add [Controller, details, and Route]

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[FrontendController::class, 'index'])->name('index');
+Route::get('/details/{slug}',[FrontendController::class, 'details'])->name('details');
+
+
 
 Route::middleware([
     'auth:sanctum',
